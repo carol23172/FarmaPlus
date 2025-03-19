1ec5919d0158f235224a305610fef509b1c18d98
Carolina <Caroliveira.almeida@gmail.com>
2025-03-19 17:03:58 -0300

    CRUD-categoria

---
+++ src/main/java/com/generation/farmaplus/model/Produto.java
@@ -0,0 +1,100 @@
+package com.generation.farmaplus.model;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+@NotBlank(message = "O atributo nome é obrigatório!")
+@Size(min = 1, max = 200, message = "O atributo nome deve ter no minímo um e no máximo 200 caracter")
+private String nome;
+
+@NotBlank(message = "O atributo fabricante é obrigatório!")
+@Size(min = 3, max = 200, message = "O atributo fabricante deve ter no minímo um e no máximo 200 caracter")
+private String fabricante;
+
+@NotNull(message = "O atributo preço é obrigatório!")
+private Long preco;
+
+@NotNull(message = "O atributo estoque é obrigatório!")
+private Long estoque;
+
+@NotNull(message = "O atributo validade é obrigatório!")
+private LocalDateTime validade;
+
+@ManyToOne
+@JsonIgnoreProperties("produto")
+    private Categoria categoria;
+
+public Categoria getCategoria() {
+        return categoria;
+    }
+
+public Long getId() {
+return id;
+}
+
+public void setId(Long id) {
+this.id = id;
+}
+
+public String getNome() {
+return nome;
+}
+
+public void setNome(String nome) {
+this.nome = nome;
+}
+
+public String getFabricante() {
+return fabricante;
+}
+
+public void setFabricante(String fabricante) {
+this.fabricante = fabricante;
+}
+
+public Long getPreco() {
+return preco;
+}
+
+public void setPreco(Long preco) {
+this.preco = preco;
+}
+
+public Long getEstoque() {
+return estoque;
+}
+
+public void setEstoque(Long estoque) {
+this.estoque = estoque;
+}
+
+public LocalDateTime getValidade() {
+return validade;
+}
+
+public void setValidade(LocalDateTime validade) {
+this.validade = validade;
+}
+
+
+
+}
--- src/main/java/com/generation/farmaplus/repository/ProdutoRepository.java
+++ src/main/java/com/generation/farmaplus/repository/ProdutoRepository.java
@@ -1,5 +1,11 @@
 package com.generation.farmaplus.repository;
 
-public class ProdutoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.farmaplus.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository <Produto, Long> {
+List<Produto> findAllByNomeContainingIgnoreCase(String nome);
 }
